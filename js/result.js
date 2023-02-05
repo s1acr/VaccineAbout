70396a3a868350c396c3986873cecd649f5dee0a
slacr <2845391871@qq.com>
2023-02-05 08:19:05 +0800

    ver 0.0.1

--- js/result.js
+++ js/result.js
@@ -2,7 +2,7 @@
  * @Author: slacr 2845391871@qq.com
  * @Date: 2023-02-02 19:40:48
  * @LastEditors: slacr 2845391871@qq.com
- * @LastEditTime: 2023-02-05 08:07:40
+ * @LastEditTime: 2023-02-05 08:18:34
  * @FilePath: \VaccineAbout\js\result.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
